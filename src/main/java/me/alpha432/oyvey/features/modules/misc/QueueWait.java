0b4d5a2b1aa9c7e53be47cb19b592114f5e1bea4
Sjnez613 <52977333+Sjnez613@users.noreply.github.com>
2021-04-26 18:50:27 -0700

    (QueueWait Module) Later I am going to figure out how to make this (HOPEFULLY)
    Later I am going to figure out how to make this (HOPEFULLY)

---
+++ src/main/java/me/alpha432/oyvey/features/modules/misc/QueueWait.java
@@ -0,0 +1,16 @@
+package me.alpha432.oyvey.features.modules.misc;
+
+import me.alpha432.oyvey.features.modules.Module;
+
+public class QueueWait extends Module {
+
+    public QueueWait() {
+        super("QueueWait", "Puts you in queue, but you can play other servers at the same time", Category.MISC, true, false, false);
+    }
+
+    public void onEnable() {
+
+        //Later I am going to figure out how to make this (HOPEFULLY)
+
+    }
+}
